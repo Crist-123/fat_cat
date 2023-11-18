433c46694be3dc2ade58f6d63f0831249e74ee02
Crist-123 <141484090+Crist-123@users.noreply.github.com>
2023-11-18 11:35:25 +0800

    Add files via upload
    PSMNet注释

---
+++ models/stackhourglass.py
@@ -0,0 +1,178 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.utils.data
+from torch.autograd import Variable
+import torch.nn.functional as F
+import math
+from .submodule import *
+
+'''
+3D CNN 部分关于 Stacker hourglass部分的视线
+hourglass有三个主要的沙漏网络，训练时赋予这三个输出不同的权重组合后整合输出 也是encoder+decoder结构
+'''
+class hourglass(nn.Module):
+    def __init__(self, inplanes):
+        super(hourglass, self).__init__()
+
+        self.conv1 = nn.Sequential(convbn_3d(inplanes, inplanes*2, kernel_size=3, stride=2, pad=1),
+                                   nn.ReLU(inplace=True))
+
+        self.conv2 = convbn_3d(inplanes*2, inplanes*2, kernel_size=3, stride=1, pad=1)
+
+        self.conv3 = nn.Sequential(convbn_3d(inplanes*2, inplanes*2, kernel_size=3, stride=2, pad=1),
+                                   nn.ReLU(inplace=True))
+
+        self.conv4 = nn.Sequential(convbn_3d(inplanes*2, inplanes*2, kernel_size=3, stride=1, pad=1),
+                                   nn.ReLU(inplace=True))
+
+        self.conv5 = nn.Sequential(nn.ConvTranspose3d(inplanes*2, inplanes*2, kernel_size=3, padding=1, output_padding=1, stride=2,bias=False),
+                                   nn.BatchNorm3d(inplanes*2)) #+conv2
+
+        self.conv6 = nn.Sequential(nn.ConvTranspose3d(inplanes*2, inplanes, kernel_size=3, padding=1, output_padding=1, stride=2,bias=False),
+                                   nn.BatchNorm3d(inplanes)) #+x
+    #下面是一个沙漏网络的输出流程 也就是每个stack的结构
+    def forward(self, x ,presqu, postsqu):
+        #见我自己的视频解说 这一块对着看代入就行
+        out  = self.conv1(x) #in:1/4 out:1/8
+        pre  = self.conv2(out) #in:1/8 out:1/8
+        if postsqu is not None:
+           pre = F.relu(pre + postsqu, inplace=True)
+        else:
+           pre = F.relu(pre, inplace=True)
+
+        out  = self.conv3(pre) #in:1/8 out:1/16
+        out  = self.conv4(out) #in:1/16 out:1/16
+
+        if presqu is not None:
+           post = F.relu(self.conv5(out)+presqu, inplace=True) #in:1/16 out:1/8
+        else:
+           post = F.relu(self.conv5(out)+pre, inplace=True) 
+
+        out  = self.conv6(post)  #in:1/8 out:1/4
+
+        return out, pre, post
+
+class PSMNet(nn.Module):
+    def __init__(self, maxdisp):
+        super(PSMNet, self).__init__()
+        self.maxdisp = maxdisp
+
+        self.feature_extraction = feature_extraction()
+
+        self.dres0 = nn.Sequential(convbn_3d(64, 32, 3, 1, 1),
+                                     nn.ReLU(inplace=True),
+                                     convbn_3d(32, 32, 3, 1, 1),
+                                     nn.ReLU(inplace=True))
+
+        self.dres1 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
+                                   nn.ReLU(inplace=True),
+                                   convbn_3d(32, 32, 3, 1, 1)) 
+
+        self.dres2 = hourglass(32)
+
+        self.dres3 = hourglass(32)
+
+        self.dres4 = hourglass(32)
+
+        self.classif1 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1,bias=False))
+
+        self.classif2 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1,bias=False))
+
+        self.classif3 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv3d(32, 1, kernel_size=3, padding=1, stride=1,bias=False))
+        #kaiming 初始化
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.Conv3d):
+                n = m.kernel_size[0] * m.kernel_size[1]*m.kernel_size[2] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm3d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+
+    def forward(self, left, right):
+
+        refimg_fea     = self.feature_extraction(left)
+        targetimg_fea  = self.feature_extraction(right)
+
+
+        #matching
+        '''
+        创建一个五维的cost张量，形状是[batch_size, 2 * channels, maxdisp//4, height, width
+        维度一：refimg_fea.size()[0]: 参考图像（左图）的第一个维度 bacthsize
+        维度2：refimg_fea.size()[1】*2：left_image的第二个维度 通道数 乘以2是为了储存左右两张图的信息
+        维度3：self.maxdisp//4 储存不同视差对应cost volumn矩阵的维度 输入图像的分辨率是1、4 H*W 相应的视差搜索范围也变为原来的1/4
+        维度4/5: 图像的H*W
+        '''
+        cost = Variable(torch.FloatTensor(refimg_fea.size()[0], refimg_fea.size()[1]*2, self.maxdisp//4,  refimg_fea.size()[2],  refimg_fea.size()[3]).zero_()).cuda()
+
+        '''
+       为每一个视差创建一个cost volumn
+       cost volumn的原理就是让目标图 沿着参考图每次滑动一个视差，滑动后将左右图进行拼接（对于目标图而言 多的部分删掉 少的部分填充0）
+       这样就相当于对不同视差建立了cost volumn 至于为什么要创建这个，实际上是为了让两个图重合点有重合的机会 这样就能学到重合部分的匹配规律特征
+        '''
+        for i in range(self.maxdisp//4):
+            if i > 0 :
+            # 从代码具体实现来看 作者应该是只提取了滑动过程中重合部分的信息 非重合部分不用参考图像的值填充，直接使用上面构建cost的初始值（也就是不重合的部分置为0）
+             cost[:, :refimg_fea.size()[1], i, :,i:]   = refimg_fea[:,:,:,i:]
+             cost[:, refimg_fea.size()[1]:, i, :,i:] = targetimg_fea[:,:,:,:-i]
+            else:
+             cost[:, :refimg_fea.size()[1], i, :,:]   = refimg_fea
+             cost[:, refimg_fea.size()[1]:, i, :,:]   = targetimg_fea
+        cost = cost.contiguous()
+
+        cost0 = self.dres0(cost)
+        cost0 = self.dres1(cost0) + cost0
+        #out1-3 对应hourglass的三个分支输出
+        out1, pre1, post1 = self.dres2(cost0, None, None) 
+        out1 = out1+cost0
+
+        out2, pre2, post2 = self.dres3(out1, pre1, post1) 
+        out2 = out2+cost0
+
+        out3, pre3, post3 = self.dres4(out2, pre1, post2) 
+        out3 = out3+cost0
+
+        #这里注意一下进行最后分类器输出的时候 前面的cost要加到后面cost中 比如out3是三个classif直接相加
+        cost1 = self.classif1(out1)
+        cost2 = self.classif2(out2) + cost1
+        cost3 = self.classif3(out3) + cost2
+
+        if self.training:
+            cost1 = F.upsample(cost1, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+            cost2 = F.upsample(cost2, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+
+            cost1 = torch.squeeze(cost1,1)
+            pred1 = F.softmax(cost1,dim=1)
+            pred1 = disparityregression(self.maxdisp)(pred1)
+
+            cost2 = torch.squeeze(cost2,1)
+            pred2 = F.softmax(cost2,dim=1)
+            pred2 = disparityregression(self.maxdisp)(pred2)
+
+        cost3 = F.upsample(cost3, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+        cost3 = torch.squeeze(cost3,1)
+        pred3 = F.softmax(cost3,dim=1)
+        #For your information: This formulation 'softmax(c)' learned "similarity" 
+        #while 'softmax(-c)' learned 'matching cost' as mentioned in the paper.
+        #However, 'c' or '-c' do not affect the performance because feature-based cost volume provided flexibility.
+        pred3 = disparityregression(self.maxdisp)(pred3)
+
+        if self.training:
+            return pred1, pred2, pred3
+        else:
+            return pred3
---
+++ models/submodule.py
@@ -0,0 +1,153 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.utils.data
+from torch.autograd import Variable
+import torch.nn.functional as F
+import math
+import numpy as np
+
+
+'''
+这个函数实现了第一块特征提取任务 包括： CNN+SPP 获得语义信息
+'''
+def convbn(in_planes, out_planes, kernel_size, stride, pad, dilation):
+    #这步关于"padding=dilation if dilation > 1 else pad"稍微解释一下：空洞卷积时padding=dialtion保持输出图像的尺寸
+    return nn.Sequential(nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=dilation if dilation > 1 else pad, dilation = dilation, bias=False),
+                         nn.BatchNorm2d(out_planes))
+
+
+def convbn_3d(in_planes, out_planes, kernel_size, stride, pad):
+
+    return nn.Sequential(nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, padding=pad, stride=stride,bias=False),
+                         nn.BatchNorm3d(out_planes))
+
+#类似resnet做的basicblock结构
+class BasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, inplanes, planes, stride, downsample, pad, dilation):
+        super(BasicBlock, self).__init__()
+        #conv0_1的stride=2 下采样一倍 输出为1/2H * 1/2W *32
+        self.conv1 = nn.Sequential(convbn(inplanes, planes, 3, stride, pad, dilation),
+                                   nn.ReLU(inplace=True))
+        self.conv2 = convbn(planes, planes, 3, 1, pad, dilation)
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+
+        if self.downsample is not None:
+            x = self.downsample(x)
+
+        out += x
+
+        return out
+
+#视差回归函数 视差等级*属于该等级视差的概率
+class disparityregression(nn.Module):
+    def __init__(self, maxdisp):
+        super(disparityregression, self).__init__()
+        self.disp = torch.Tensor(np.reshape(np.array(range(maxdisp)),[1, maxdisp,1,1])).cuda()
+
+    def forward(self, x):
+        out = torch.sum(x*self.disp.data,1, keepdim=True)
+        return out
+
+# 输入图像大小为256*512
+class feature_extraction(nn.Module):
+    def __init__(self):
+        super(feature_extraction, self).__init__()
+        self.inplanes = 32
+
+        # 仿照resnet结构，做的CNN特征提取
+        #conv0_1 + conv0_2 + conv0_3
+        self.firstconv = nn.Sequential(convbn(3, 32, 3, 2, 1, 1),
+                                       nn.ReLU(inplace=True),
+                                       convbn(32, 32, 3, 1, 1, 1),
+                                       nn.ReLU(inplace=True),
+                                       convbn(32, 32, 3, 1, 1, 1),
+                                       nn.ReLU(inplace=True))
+        #conv1 conv2 conv3 conv4 layer4输出的图像大小为  1/4H * 1/4w *128 也就是64*128
+        self.layer1 = self._make_layer(BasicBlock, 32, 3, 1,1,1)
+        self.layer2 = self._make_layer(BasicBlock, 64, 16, 2,1,1) 
+        self.layer3 = self._make_layer(BasicBlock, 128, 3, 1,1,1)
+        self.layer4 = self._make_layer(BasicBlock, 128, 3, 1,1,2)
+
+        '''
+        SSP Module:
+        整体是为了 从局部到全局的特征整合
+        使用不同面积的平均池化层，kernal越大 感受野越大 通过四个branch（注意图片）获得不同感受野 不同层次的特征，其实这种金字塔模型
+        这显然不是深度估计任务特有的结构 整合语义信息经常可以使用
+        '''
+        #1*1卷积核降维减少参数   统一四个branch到32维 用于后续concat
+        #注意torch.cat不会新增维度，比如三维矩阵torch.cat后还是三维矩阵，但行数直接相加，相当于通过增加每一维的行数在每一个维度有了更多的信息
+        self.branch1 = nn.Sequential(nn.AvgPool2d((64, 64), stride=(64,64)),
+                                     convbn(128, 32, 1, 1, 0, 1),
+                                     nn.ReLU(inplace=True))
+
+        self.branch2 = nn.Sequential(nn.AvgPool2d((32, 32), stride=(32,32)),
+                                     convbn(128, 32, 1, 1, 0, 1),
+                                     nn.ReLU(inplace=True))
+
+        self.branch3 = nn.Sequential(nn.AvgPool2d((16, 16), stride=(16,16)),
+                                     convbn(128, 32, 1, 1, 0, 1),
+                                     nn.ReLU(inplace=True))
+
+        self.branch4 = nn.Sequential(nn.AvgPool2d((8, 8), stride=(8,8)),
+                                     convbn(128, 32, 1, 1, 0, 1),
+                                     nn.ReLU(inplace=True))
+
+        self.lastconv = nn.Sequential(convbn(320, 128, 3, 1, 1, 1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(128, 32, kernel_size=1, padding=0, stride = 1, bias=False))
+
+    def _make_layer(self, block, planes, blocks, stride, pad, dilation):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            #resnet中用于某些layer由于维度较输入维度发生变化，进行残差连接时需要用1*1卷积核对输入layer的原图进行升维处理
+           downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),)
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, pad, dilation))
+        #每次inplanes改变
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes,1,None,pad,dilation))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        #step1: CNN
+        output      = self.firstconv(x)
+        output      = self.layer1(output)
+        output_raw  = self.layer2(output)
+        output      = self.layer3(output_raw)
+        output_skip = self.layer4(output)
+
+
+        output_branch1 = self.branch1(output_skip)
+        #图像大小也要统一，这里统一到 1/4H * 1/4W
+        output_branch1 = F.upsample(output_branch1, (output_skip.size()[2],output_skip.size()[3]),mode='bilinear')
+
+        output_branch2 = self.branch2(output_skip)
+        output_branch2 = F.upsample(output_branch2, (output_skip.size()[2],output_skip.size()[3]),mode='bilinear')
+
+        output_branch3 = self.branch3(output_skip)
+        output_branch3 = F.upsample(output_branch3, (output_skip.size()[2],output_skip.size()[3]),mode='bilinear')
+
+        output_branch4 = self.branch4(output_skip)
+        output_branch4 = F.upsample(output_branch4, (output_skip.size()[2],output_skip.size()[3]),mode='bilinear')
+        #拼接处理
+        output_feature = torch.cat((output_raw, output_skip, output_branch4, output_branch3, output_branch2, output_branch1), 1)
+        output_feature = self.lastconv(output_feature)
+
+        return output_feature
+
+
+
